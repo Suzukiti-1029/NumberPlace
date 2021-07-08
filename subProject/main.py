8bd19b8c18603117b2aa47eab2b1dc931f5affde
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-07-08 20:38:17 +0900

    [試作]コード・処理の最適化4

--- subProject/main.py
+++ subProject/main.py
@@ -19,18 +19,18 @@ for y in range(2):
         width=3
     )
 
-def side14_num_checker(W_x_index, W_y_index):
-  for x1 in range(W_x_index, W_x_index + 4):
-    for x2 in range(W_x_index, W_x_index + 4):
+def side_checker(W_y_index, grip_step):
+  for x1 in range(0, grip_step):
+    for x2 in range(0, grip_step):
       if x1 == x2:
         continue
       if grid[W_y_index][x1] == grid[W_y_index][x2]:
         return False
   return True
 
-def vertical41_num_checker(N_x_index, N_y_index):
-  for y1 in range(N_y_index, N_y_index + 4):
-    for y2 in range(N_y_index, N_y_index + 4):
+def vertical_checker(N_x_index, grip_step):
+  for y1 in range(0, grip_step):
+    for y2 in range(0, grip_step):
       if y1 == y2:
         continue
       if grid[y1][N_x_index] == grid[y2][N_x_index]:
@@ -51,25 +51,24 @@ while True:
     else:
       grid[y][x] = number_list3.pop(rd.randint(0, len(number_list3)-1))
 
-  if side14_num_checker(0, 0) and vertical41_num_checker(0, 0):
+  if side_checker(0, 4) and vertical_checker(0, 4):
     for [y, x] in [[1, 2], [1, 3], [2, 1], [3, 1]]:
       if y == 1:
         grid[y][x] = number_list2.pop(rd.randint(0, len(number_list2)-1))
       else:
         grid[y][x] = number_list3.pop(rd.randint(0, len(number_list3)-1))
 
-    if side14_num_checker(0, 1) and vertical41_num_checker(1, 0):
+    if side_checker(1, 4) and vertical_checker(1, 4):
       number_list4 = [i for i in range(1, 5)]
       for y in range(2, 4):
         for x in range(2, 4):
           grid[y][x] = number_list4.pop(rd.randint(0, len(number_list4)-1))
 
-      if side14_num_checker(0, 2) and side14_num_checker(0, 3) \
-      and vertical41_num_checker(2, 0) and vertical41_num_checker(3, 0):
+      if side_checker(2, 4) and side_checker(3, 4) and vertical_checker(2, 4) and vertical_checker(3, 4):
         break
 
   grid = [[0 for j in range(4)] for i in range(4)]
-
+"""
 random_num_list = []
 for i in range(4):
   for j in range(4):
@@ -77,7 +76,7 @@ for i in range(4):
 for a in range(9):
   [x, y] = random_num_list.pop(rd.randint(0, len(random_num_list)-1))
   grid[y][x] = ''
-
+"""
 for q in range(4):
   for p in range(4):
     label = tk.Label(root, text=grid[q][p], font=('System', 56))
