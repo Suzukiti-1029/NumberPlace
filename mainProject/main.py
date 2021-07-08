955e7996e9053d4416cd1286369cbebdbdbfb640
Suzukiti-1029 <h40396suzujun@gmail.com>
2021-07-08 21:53:29 +0900

    数の配置のルール判定、空白マスを生成

--- mainProject/main.py
+++ mainProject/main.py
@@ -1,6 +1,32 @@
 import tkinter as tk
 import random as rd
 
+def num_checker(index, grid_step):
+  # side_checker
+  for x1 in range(0, grid_step):
+    for x2 in range(0, grid_step):
+      if x1 == x2:
+        continue
+      if grid[index][x1] == grid[index][x2]:
+        return False
+  # vertical_checker
+  for y1 in range(0, grid_step):
+    for y2 in range(0, grid_step):
+      if y1 == y2:
+        continue
+      if grid[y1][index] == grid[y2][index]:
+        return False
+  return True
+
+def create_vacant(vacant_numbers, grid_step):
+  random_num_list = []
+  for i in range(grid_step):
+    for j in range(grid_step):
+      random_num_list.append([i, j])
+  for n in range(vacant_numbers):
+    [x, y] = random_num_list.pop(rd.randint(0, len(random_num_list) - 1))
+    grid[y][x] = ''
+
 def create_grid(grid_step):
   cvs = tk.Canvas(
     width = 40 + grid_step * 80,
@@ -35,8 +61,12 @@ def create_grid(grid_step):
         anchor=tk.CENTER
       )
 
-
 grid = [[0 for j in range(9)] for i in range(9)]
+
+while True:
+  break
+
+# create_vacant(28, 9)
 
 root = tk.Tk()
 root.title('3✕3マスの数独')
